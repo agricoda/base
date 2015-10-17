b570f2c83da532b03d1b1bf9dc96a22a2a547f26
James <admin@agcloud.nz>
2015-10-17 21:14:22 +1300

    Added template ability. Pre-Kernel.

--- src/app.php
+++ src/app.php
@@ -1,9 +1,41 @@
 <?php
+// example.com/src/app.php
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+
+/**
+ *
+ *
+ */
+class LeapYearController
+{
+
+    public function indexAction($request)
+    {
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this a leap year!');
+        }
+
+        return new Response('Nah, not a leaper!');
+    }
+}
+
+
+function is_leap_year($year = null)
+{
+    if (null === $year) {
+        $year = date('Y');
+    }
+
+    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
+
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'world')));
-$routes->add('bye', new Routing\Route('/bye'));
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+  'year'        => null,
+  '_controller' => array(new LeapYearController(), 'indexAction'),
+)));
 
 return $routes;
